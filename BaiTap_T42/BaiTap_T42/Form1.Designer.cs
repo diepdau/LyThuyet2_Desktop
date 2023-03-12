d18bacbba01dd5c4e133acc1e2ed1b23906e7e73
diepdau <92206593+diepdau@users.noreply.github.com>
2023-03-12 09:49:59 +0700

    Add files via upload

---
+++ BaiTap_T42/BaiTap_T42/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace BaiTap_T42
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "89031617_1068493156848540_5119610345500966912_n.jpg");
+			this.imageList1.Images.SetKeyName(1, "87399665_956348898093885_8422309934061846528_n.jpg");
+			this.imageList1.Images.SetKeyName(2, "87799463_260603511601298_6531372173074890752_n.jpg");
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(26, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(330, 273);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.ImageIndex = 2;
+			this.button1.ImageList = this.imageList1;
+			this.button1.Location = new System.Drawing.Point(404, 22);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(37, 32);
+			this.button1.TabIndex = 2;
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(456, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(42, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Mặt trời";
+			// 
+			// button2
+			// 
+			this.button2.ImageIndex = 1;
+			this.button2.ImageList = this.imageList1;
+			this.button2.Location = new System.Drawing.Point(404, 132);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(37, 32);
+			this.button2.TabIndex = 4;
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.ImageIndex = 0;
+			this.button3.ImageList = this.imageList1;
+			this.button3.Location = new System.Drawing.Point(404, 76);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(37, 32);
+			this.button3.TabIndex = 5;
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(456, 142);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Hoa lan";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(456, 95);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(27, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Hoa";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ BaiTap_T48/BaiTap_T48/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BaiTap_T48
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.biafToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.bai12ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.bai13ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.bài112ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.bài113ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+			this.bài14ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.biafToolStripMenuItem,
+            this.toolStripComboBox1});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(129, 450);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// biafToolStripMenuItem
+			// 
+			this.biafToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bai12ToolStripMenuItem,
+            this.bai13ToolStripMenuItem,
+            this.bài14ToolStripMenuItem});
+			this.biafToolStripMenuItem.Name = "biafToolStripMenuItem";
+			this.biafToolStripMenuItem.Size = new System.Drawing.Size(44, 23);
+			this.biafToolStripMenuItem.Text = "Bai 1";
+			this.biafToolStripMenuItem.Click += new System.EventHandler(this.biafToolStripMenuItem_Click);
+			// 
+			// bai12ToolStripMenuItem
+			// 
+			this.bai12ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bài112ToolStripMenuItem});
+			this.bai12ToolStripMenuItem.Name = "bai12ToolStripMenuItem";
+			this.bai12ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.bai12ToolStripMenuItem.Text = "Bai 1.2";
+			// 
+			// bai13ToolStripMenuItem
+			// 
+			this.bai13ToolStripMenuItem.Name = "bai13ToolStripMenuItem";
+			this.bai13ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.bai13ToolStripMenuItem.Text = "Bai 1.3";
+			// 
+			// bài112ToolStripMenuItem
+			// 
+			this.bài112ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bài113ToolStripMenuItem});
+			this.bài112ToolStripMenuItem.Name = "bài112ToolStripMenuItem";
+			this.bài112ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.bài112ToolStripMenuItem.Text = "Bài 1.1.2";
+			// 
+			// bài113ToolStripMenuItem
+			// 
+			this.bài113ToolStripMenuItem.Name = "bài113ToolStripMenuItem";
+			this.bài113ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.bài113ToolStripMenuItem.Text = "Bài 1.1.3";
+			// 
+			// toolStripComboBox1
+			// 
+			this.toolStripComboBox1.Name = "toolStripComboBox1";
+			this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+			this.toolStripComboBox1.Click += new System.EventHandler(this.toolStripComboBox1_Click);
+			// 
+			// bài14ToolStripMenuItem
+			// 
+			this.bài14ToolStripMenuItem.Name = "bài14ToolStripMenuItem";
+			this.bài14ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.bài14ToolStripMenuItem.Text = "Bài 1.4";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Click += new System.EventHandler(this.toolStripComboBox1_Click);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem biafToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem bai12ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem bai13ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem bài112ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem bài113ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem bài14ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+	}
+}
+
